da6235dd39a050b9aac03a783aee52048332e92a
Vladlen Shaparev <shve@outlook.com>
2022-04-29 16:04:02 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using TwoWords; //Why it doesn't work?
+
+Console.WriteLine("Please, enter first word.");
+var firstWord = Console.ReadLine();
+Console.WriteLine("Please, enter second word.");
+var secondWord = Console.ReadLine();
+
+Console.WriteLine("Intersection of two words = ");
+foreach (var ch in TwoWords.TwoWords.FindIntersection(firstWord, secondWord))
+{
+    Console.Write(" " + ch);
+}
+
+Console.WriteLine("\nYou can" + (TwoWords.TwoWords.CheckForReconstruction(firstWord, secondWord)? "":"'t")
+    + " make second word of letters taken from first word.");
---
+++ TwoWords.cs
@@ -0,0 +1,29 @@
+﻿namespace TwoWords
+{
+    internal static class TwoWords
+    {
+        internal static bool CheckForReconstruction(string firstWord, string secondWord)
+        {
+            var firstCharArray = firstWord.ToCharArray().ToList();
+            foreach (var ch in secondWord.ToCharArray())
+            {
+                if (firstCharArray.Contains(ch))
+                {
+                    firstCharArray.Remove(ch);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        internal static char[] FindIntersection(string firstWord, string secondWord)
+        {
+            var firstArray = firstWord.ToCharArray();
+            var secondArray = secondWord.ToCharArray();
+            return firstArray.Intersect(secondArray).ToArray();
+        }
+    }
+}
